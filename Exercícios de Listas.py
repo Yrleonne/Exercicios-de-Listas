c66ecb95ebf2b6c0d832007c7dcd1aaf094cbfda
Yrleonne Albuquerque <yrleonne@gmail.com>
2025-08-29 14:24:07 +0000

    Exercícios de Listas

---
+++ Exercícios de Listas.py
@@ -0,0 +1,45 @@
+# 1- Crie uma lista chamada livros contendo 3 livros diferentes e exiba a lista na tela
+#livros = ['Harry Potter', 'O Pequeno Principe', 'O Senhor dos Aneis ']
+#print(livros)
+
+# 2- Usando a lista livros, exiba apenas o primeiro e o último elemento.
+#livros = ['Harry Potter', 'O Pequeno Principe', 'O Senhor dos Aneis ']
+#print(livros[0], livros[2])
+
+# 3- Adicione mais dois livros à lista livros usando o método append() e exiba a lista atualizada
+#livros = ['Harry Potter', 'O Pequeno Principe', 'O Senhor dos Aneis ']
+#livros.append('Pinoquio'),livros.append('Chapeuzinho')
+#print(livros)
+
+# 4- Insira o livro "Duna" na segunda posição da lista livros usando insert().
+#livros = ['Harry Potter', 'O Pequeno Principe', 'O Senhor dos Aneis ']
+#livros.insert(1, 'Duna')
+#print(livros)
+
+# 5- Remova o livro "Silêncio dos inocentes" da lista usando remove(). Se ele não existir, exiba a mensagem "Livro não encontrado".
+#livros = ['Harry Potter', 'O Pequeno Principe', 'O Senhor dos Aneis ']
+#livros.remove('Silencio dos inocentes')
+#print(livros)
+
+# 6- Crie uma lista chamada números com os valores [1, 2, 3, 2, 4, 2, 5]. Mostre quantas vezes o número 2 aparece na lista.
+#numeros = [1,2,3,2,4,2,5]
+#quantidade = numeros.count(2)
+#print('numero 2 aparece', quantidade, 'vezes.')
+
+# 7- Percorra a lista livros e exiba cada livro com a frase: "O livro <nome do livro> é interessante"
+#livros = ['Harry Potter', 'O Pequeno Principe', 'O Senhor dos Aneis ']
+#for livro in livros:
+#    print(f'o livro {livro} é interessante')
+
+#8- Dada a lista idades = [12, 18, 25, 14, 30], use um laço para exibir somente as idades maiores ou iguais a 18.
+#idades = [12,18,25,14,30]
+#for idade in idades:
+#    if idade >= 18:
+#        print(idade)
+
+# 9- Crie uma lista valores = [10, 20, 30, 40]. Use um laço for para calcular manualmente a soma de todos os valores.
+
+
+
+
+#livros = ['Harry Potter', 'O Pequeno Principe', 'O Senhor dos Aneis ']
